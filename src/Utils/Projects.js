90afdaf97a582fbb5e62fd1f6738ac88ba8ac003
Prashant-SL <pacchidon07@gmail.com>
2022-03-16 19:28:37 +0530

    Portfolio

---
+++ src/Utils/Projects.js
@@ -0,0 +1,30 @@
+// import Purplle from '../assets/purplle.png'
+// var prefetch = new Image();
+//prefetch.src = "../assets/purplle.png";
+export const projects = [
+	{
+		img: 'https://raw.githubusercontent.com/ankush4gupta/ajio_backend/Prashant-SL/AJIO%20CLONE%20HOMEPAGE.jpeg',
+		title: 'Ajio.com Clone',
+		des: 'This is a team project where we worked on cloning the Ajio website. Here users can buy fashion & lifestyle products. We created the following things Navbar, Home Page, Product Page, Cart Page, Payment Page, Wishlist Page. A team of 5 members, done in 5 days',
+		demo: '',
+		live: 'https://ajio-masai.herokuapp.com/',
+		gitHub: 'https://github.com/ankush4gupta/ajio_backend',
+		technologies: [
+			'HTML',
+			'CSS',
+			'JavaScript',
+			'Node.js',
+			'Express.js',
+			'MongoDB',
+		],
+	},
+	{
+		img: 'https://raw.githubusercontent.com/Niks14041/Pantloons-clone/prashant/Pantaloons%20Clone.jpeg',
+		title: 'Pantaloons.com Clone',
+		des: "Pantaloons.com is an Online Shopping Site for Men's and Women's Clothing. We built interface using HTML, CSS & DOM where users can add items to the cart, use filters to check items by Price and category, and added login functionality.",
+		demo: '',
+		live: 'https://pantaloons-clone-project.netlify.app/homepage.html',
+		gitHub: 'https://github.com/Niks14041/Pantloons-clone',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'DOM'],
+	},
+];
